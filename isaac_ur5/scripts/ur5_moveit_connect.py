cff3521eaeccef8e6a0d1507e5bb9d66f321ac7e
alpharomeo911 <sarvesh101p@gmail.com>
2023-05-12 18:09:54 -0400

    added moveit and moveit connection

---
+++ isaac_ur5/scripts/ur5_moveit_connect.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import rospy
+from sensor_msgs.msg import JointState
+
+joints_dict = {}
+
+
+def joint_states_callback(message):
+    rospy.loginfo(message.position[0])
+    joint_commands = JointState()
+
+    joint_commands.header = message.header
+
+    for i, name in enumerate(message.name):
+        joints_dict[name] = message.position[i]
+
+    joint_commands.name = joints_dict.keys()
+    joint_commands.position = joints_dict.values()
+    pub.publish(joint_commands)
+
+    return
+
+
+if __name__ == "__main__":
+    rospy.init_node("ur5_moveit_connect")
+    pub = rospy.Publisher("/joint_command", JointState, queue_size=1)
+    rospy.Subscriber("/move_group/fake_controller_joint_states", JointState, joint_states_callback, queue_size=10)
+    rospy.spin()
